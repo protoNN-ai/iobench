841268f986be4bd2c11c23edfb64efebe5b521f2
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-12-07 13:35:26 +0900

    measure time to list files

--- iobench/__main__.py
+++ iobench/__main__.py
@@ -1,14 +1,28 @@
 """iobench CLI"""
+import sys
+import os
+import logging
+from timeit import default_timer as timer
+from ._version import VERSION
 
+LOGGER = logging.getLogger(__name__)
+logging.basicConfig(level="INFO")
 
 def main():
     """entry point for the iobench CLI"""
-    print("hi")
+    print("iobench v " + str(VERSION))
+    if len(sys.argv) < 2:
+        print("specify path")
+    path = sys.argv[1]
+    LOGGER.info("reading list of files in directory")
+    time_start = timer()
+    files = [os.path.join(path, f) for f in os.listdir(path)]
+    time_end = timer()
+    LOGGER.info("{} files listed in {} s".format(str(len(files)), time_end - time_start))
 
-    # parse command line args
-    # read list of file names
-    # shuffle file names across workers
-    # each worker reads its portion of files
+
+    # TODO: shuffle file names across workers
+    # TODO: make each worker read its portion of files
 
 
 if __name__ == "__main__":
