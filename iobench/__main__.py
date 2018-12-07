2eb92b800429ed1186ee6e28e1ccc467072a71db
Aleksandr Drozd <alexander.drozd@gmail.com>
2018-12-07 11:26:20 +0900

    outline todos

--- iobench/__main__.py
+++ iobench/__main__.py
@@ -5,6 +5,11 @@ def main():
     """entry point for the iobench CLI"""
     print("hi")
 
+    # parse command line args
+    # read list of file names
+    # shuffle file names across workers
+    # each worker reads its portion of files
+
 
 if __name__ == "__main__":
     main()
